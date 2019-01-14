f5ae16bc9479369194314dc6a55924f8535977e9
Bailey Diveley <bailey.diveley@gmail.com>
2019-01-14 14:18:54 -0700

    Add functionality to all buttons on page

--- number_guesser.js
+++ number_guesser.js
@@ -4,7 +4,7 @@ A button for submitting a guess X
 A button for clearing the input field X
 A button that resets the game X
 User’s Guess State:
-Display the user’s most recent guess
+Display the user’s most recent guess X
 Display results and feedback:
 If their guess is too high, it should display: “That is too high”
 If their guess is too low, it should display: “That is too low”
@@ -14,3 +14,31 @@ guessNumber()
 clearGuess()
 resetGame()
 */
+
+function randomizeNumber() {
+  Math.floor((Math.random() * 100) + 1);
+}
+
+function guessNumber() {
+  var x = document.getElementById("guess").value;
+  text = `Your last guess was ${x}`;
+  document.getElementById("response").innerHTML = text;
+}
+
+function clearGuess() {
+  document.getElementById("guess").value = " ";
+}
+
+function clearResponse() {
+  document.getElementById("response").innerHTML = " ";
+}
+
+function resetGame() {
+  clearGuess();
+  clearResponse();
+  playGame();
+}
+
+function playGame() {
+
+}
