cc231aceeae207951f72ad45940bbbfe17ffec60
Bailey Diveley <bailey.diveley@gmail.com>
2019-01-14 13:47:42 -0700

    Add buttons and text field and some basic styling

--- number_guesser.js
+++ number_guesser.js
@@ -1,12 +1,16 @@
 /* Zero State:
-An input field for guessing the number
-A button for submitting a guess
-A button for clearing the input field
-A button that resets the game
+An input field for guessing the number  X
+A button for submitting a guess X
+A button for clearing the input field X
+A button that resets the game X
 User’s Guess State:
 Display the user’s most recent guess
 Display results and feedback:
 If their guess is too high, it should display: “That is too high”
 If their guess is too low, it should display: “That is too low”
 If the guess is correct, it should display: “BOOM!”
+
+guessNumber()
+clearGuess()
+resetGame()
 */
