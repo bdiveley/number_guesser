113a29140a2bfd65d1c677362a33fa1bbf5f4fd3
Bailey Diveley <bailey.diveley@gmail.com>
2019-01-14 13:02:05 -0700

    initial file setup

---
+++ number_guesser.js
@@ -0,0 +1,12 @@
+/* Zero State:
+An input field for guessing the number
+A button for submitting a guess
+A button for clearing the input field
+A button that resets the game
+User’s Guess State:
+Display the user’s most recent guess
+Display results and feedback:
+If their guess is too high, it should display: “That is too high”
+If their guess is too low, it should display: “That is too low”
+If the guess is correct, it should display: “BOOM!”
+*/
