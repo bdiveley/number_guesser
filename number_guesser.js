a152403b8ad90cea27f5b0592b843bf0baea7d78
Bailey Diveley <bailey.diveley@gmail.com>
2019-01-14 19:43:11 -0700

    Add user input for min and max and allow for incresae range after each successful guess

--- number_guesser.js
+++ number_guesser.js
@@ -1,10 +1,11 @@
 function randomizeNumber() {
-  return Math.floor((Math.random() * maximum) + minimum);
+  min = Math.ceil(minimum);
+  max = Math.floor(maximum);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function guessNumber() {
-  var guessed = document.getElementById("guess").value;
-  setMinMax();
+  var guessed = parseInt(document.getElementById("guess").value);
   if (validGuess(guessed)) {
     enableBtn("resetbtn");
     enableBtn("clearbtn");
@@ -13,16 +14,18 @@ function guessNumber() {
   };
 }
 
-function setMinMax() {
-  minimum = document.getElementById("minimum").value;
-  maximum = document.getElementById("maximum").value;
+function setGameVariables() {
+  minimum = parseInt(document.getElementById("minimum").value);
+  maximum = parseInt(document.getElementById("maximum").value);
+  answer = randomizeNumber();
+  disableBtn("rangebtn");
 }
 
 function validGuess(guess) {
   if (isNaN(guess)) {
     alert("You must enter a number");
     return false;
-  } else if (parseInt(guess) < minimum || parseInt(guess) > maximum) {
+  } else if (guess < minimum || guess > maximum) {
     alert("Number must be within range");
     return false;
   } else {
@@ -37,7 +40,8 @@ function compareNumbers(guess) {
   } else if (guess > answer) {
     return `${text} That is too high`;
   } else {
-    return "BOOM!";
+    levelUp();
+    return `BOOM!  Your just leveled up!  Your new guess range is ${minimum} to ${maximum}`;
   };
 }
 
@@ -55,13 +59,21 @@ function clearRange() {
   document.getElementById("maximum").value = "";
 }
 
+function levelUp() {
+  minimum -= 10;
+  maximum += 10;
+  document.getElementById("minimum").value = minimum;
+  document.getElementById("maximum").value = maximum;
+  answer = randomizeNumber();
+}
+
 function resetGame() {
   clearGuess();
   clearResponse();
   clearRange();
   disableBtn("resetbtn");
   disableBtn("clearbtn");
-  answer = randomizeNumber();
+  enableBtn("rangebtn")
 }
 
 function disableBtn(button) {
@@ -72,6 +84,6 @@ function enableBtn(button) {
   document.getElementById(button).disabled = false;
 }
 
-var answer = randomizeNumber();
+var answer = 0;
 var minimum = 0;
 var maximum = 0;
